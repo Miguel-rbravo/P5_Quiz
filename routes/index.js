4823935a6d72830ec3934e28d1d16e0e39190665
Miguel-rbravo <miguel.rbravo@alumnos.upm.es>
2018-05-03 19:22:30 +0200

    P5_Quiz

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+const Sequelize = require("sequelize");
+const {models}=require('../models');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Quiz' });
+});
+router.get('/credits', function(req, res, next) {
+    res.render('credits', { title: 'Miguel Rubio' });
+});
+router.get('/quizzes', function(req, res, next) {
+    models.quiz.findAll()
+        .then(quizzes =>{
+            res.render('quizzes',{quizzes});
+        })
+        .catch(error=> next(error))
+});
+
+module.exports = router;
